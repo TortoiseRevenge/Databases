7683cc0b2881010922ff6c003dd35b2a9cf1ed8b
Andrew <22prudla1@elmbrookstudents.org>
2021-06-22 15:01:43 -0500

    container v1

---
+++ CodeTheWay/CodeTheWay.Web.Ui/Repositories/ShippingContainerRepository.cs
@@ -0,0 +1,20 @@
+﻿using CodeTheWay.Web.Ui.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodeTheWay.Web.Ui.Repositories
+{
+    public class ShippingContainerRepository : IShippingContainerRepository
+    {
+        private AppDbContext AppDbContext;
+
+        public ShippingContainerRepository(AppDbContext dbContext)
+        {
+            this.AppDbContext = dbContext;
+        }
+
+    }
+}
