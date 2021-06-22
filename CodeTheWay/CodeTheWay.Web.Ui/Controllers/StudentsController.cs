8bbe410cedb2dba26404b825dbd53d2dde58eb65
Joel Kuriakose <1joel.kuriakose@gmail.com>
2021-06-22 13:31:13 -0500

    Age

--- CodeTheWay/CodeTheWay.Web.Ui/Controllers/StudentsController.cs
+++ CodeTheWay/CodeTheWay.Web.Ui/Controllers/StudentsController.cs
@@ -1,4 +1,5 @@
-﻿using CodeTheWay.Web.Ui.Models;
+﻿using CodeTheWay.Web.Ui.Models.ViewModels;
+using CodeTheWay.Web.Ui.Models;
 using CodeTheWay.Web.Ui.Services;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -22,38 +23,79 @@ namespace CodeTheWay.Web.Ui.Controllers
         }
         public async Task<IActionResult> Create()
         {
-            return View(new Student());
+            
+            return View(new StudentRegistrationViewModel());
         }
         [HttpPost]
-        public async Task<IActionResult> Register(Student model)
+        public async Task<IActionResult> Register(StudentRegistrationViewModel model)
         {
+           
             if (ModelState.IsValid)
             {
-                var student = await StudentService.Create(model);
-                return RedirectToAction("Index");
+                if (model.Age < 19)
+                {
+                    Student student = new Student()
+                    {
+                        Id = model.Id,
+                        LastName = model.LastName,
+                        FirstMidName = model.FirstMidName,
+                    };
+                    
+                    var result = await StudentService.Create(student);
+                    return RedirectToAction("Index");
+                }
             }
             return View(model);
         }
         public async Task<IActionResult> Edit(Guid id)
         {
             var student = await StudentService.GetStudent(id);
-            return View(student);
+            StudentRegistrationViewModel stud = new StudentRegistrationViewModel()
+            {
+                Age = 0,
+                Id = id,
+                FirstMidName = student.FirstMidName,
+                LastName = student.LastName,
+            };
+
+
+            return View(stud);
         }
         [HttpPost]
-        public async Task<IActionResult> UpDate(Student model)
+        public async Task<IActionResult> UpDate(StudentRegistrationViewModel model)
         {
             if (ModelState.IsValid)
             {
-                var student = await StudentService.Update(model);
-                return RedirectToAction("Index");
+                if (model.Age < 19)
+                {
+                    Student stud = new Student()
+                    {
+                        Id = model.Id,
+                        FirstMidName = model.FirstMidName,
+                        LastName = model.LastName,
+
+                    };
+
+                    var result = await StudentService.Update(stud);
+                    return RedirectToAction("Index");
+                }
             }
             return View(model);
         }
 
         public async Task<IActionResult> Details(Guid id)
         {
+
             var student = await StudentService.GetStudent(id);
-            return View(student);
+            StudentRegistrationViewModel stud = new StudentRegistrationViewModel()
+            {
+                Age = 0,
+                FirstMidName = student.FirstMidName,
+                LastName = student.LastName,
+                Id = student.Id,
+            };
+            
+            return View(stud);
         }
         public async Task<IActionResult> Delete(Guid id)
         {
